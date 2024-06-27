0b017c07976707ba459517dbddd900046638c59e
PiyushKoranne <pkoranne.businessmail@gmail.com>
2024-06-27 19:43:46 +0530

    latest 27th June

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './common/Navbar'
 import Footer from './common/Footer'
-import { Calendar, Radio } from 'antd';
+import { Calendar, Checkbox, Radio } from 'antd';
+import { Formik } from 'formik';
+import { HostedForm } from 'react-acceptjs';
+import { FormComponent, FormContainer } from "react-authorize-net";
+import axios from 'axios';
 
 function Home() {
+
+	const authData = {
+		apiLoginID: '5KP3u95bQpv',
+		clientKey: '346HZ32z3fP4hTG2',
+	};
 	const [preRegistrationForm, setPreRegistrationForm] = useState([
 		{
 			que: "Are you 50 years of age or older?(Required)",
@@ -36,12 +45,22 @@ function Home() {
 	const [currentQ, setCurrentQ] = useState(0);
 	const [quizComplete, setQuizComplete] = useState(false);
 	const [quizError, setQuizError] = useState(false);
+	const [formToken, setFormToken] = useState('');
 
 	function reAttemptTest() {
 		setQuizComplete(false);
 		setCurrentQ(0);
 		setQuizError(false);
 	}
+
+	async function getFormToken() {
+		const response = await axios.get("http://192.168.16.36:4001/colo-pay");
+		if (response.status === 200) {
+			setFormToken(response.data.code.token);
+		}
+	}
+
+	console.log("form token", formToken);
 
 	function setCurrentAnswer(ans) {
 		const index = currentQ;
@@ -69,7 +88,6 @@ function Home() {
 		if (quizComplete) {
 			// last question done, begin evaluation
 			const responseString = preRegistrationForm.map(item => item.ans).join("");
-			console.log("I N H E R E", responseString);
 			if (responseString !== 'yynnn') {
 				setQuizError(true);
 				setQuizComplete(true);
@@ -79,6 +97,10 @@ function Home() {
 			}
 		}
 	}, [quizComplete])
+
+	useEffect(() => {
+		getFormToken();
+	}, [])
 
 	return (
 		<>
@@ -173,7 +195,7 @@ function Home() {
 					<div className='flex items-center justify-center w-full'>
 						<div className='w-[60%]'>
 							<h3 className='font-bold text-[16px]'>Pick a Date & Time</h3>
-							<Calendar fullscreen={false} onChange={onPanelChange} />
+							<Calendar fullscreen={true} onChange={onPanelChange} />
 						</div>
 					</div>
 				</div>
@@ -195,70 +217,173 @@ function Home() {
 				<div className=' center-wr flex flex-col items-center justify-center pt-[50px] pb-[150px]'>
 					<div className='flex items-center justify-center w-full'>
 						<div className='w-[60%]'>
-							<form className='grid grid-cols-6 gap-3 gap-y-6' action="">
-								{/* First Name */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-3'>
-									<input type="text" placeholder='First Name' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* Last Name */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-3'>
-									<input type="text" placeholder='Last Name' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* Addr */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-6'>
-									<input type="text" placeholder='Street Address' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* City */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-2'>
-									<input type="text" placeholder='City' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* State */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-2'>
-									<input type="text" placeholder='State' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* Zip */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-2'>
-									<input type="text" placeholder='Zip' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* Phone */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-6'>
-									<input type="text" placeholder='Phone' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* Email */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-6'>
-									<input type="text" placeholder='Email' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* Bday */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-6'>
-									<input type="text" placeholder='Birthday (mm/dd/yyyy)' className='outline-none width-full border-none bg-transparent' />
-								</div>
-								{/* Race */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-6'>
-									<p>Race</p>
-									<div className='flex flex-wrap items-start mt-[10px] gap-[20px] gap-y-[30px]'>
-										<Radio.Group size='large' name="radiogroup" className='flex items-center justify-start gap-[20px] flex-wrap ' defaultValue={1}>
-											<Radio value={"American Indian"}>American Indian</Radio>
-											<Radio value={"Alaskan Native"}>Alaskan Native</Radio>
-											<Radio value={"Asian"}>Asian</Radio>
-											<Radio value={"White"}>White</Radio>
-											<Radio value={"African American"}>African American</Radio>
-											<Radio value={"Native American"}>Native American</Radio>
-											<Radio value={"Pacific Islander"}>Pacific Islander</Radio>
-											<Radio value={"Other"}>Other</Radio>
-										</Radio.Group>
+							<Formik
+								initialValues={{ firstName: '', lastName: '', dob: '', streetAddress: "", city: '', state: '', zip: '', phone: '', email: '', race: 'American Indian', ethnicity: 'Hispanic/Latino' }}
+								validate={values => {
+									const errors = {};
+									if (!values.email) {
+										errors.email = 'Required';
+									} else if (
+										!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+									) {
+										errors.email = 'Invalid email address';
+									}
+									if (!values.firstName) errors.firstName = "Please enter your first name";
+									if (!values.lastName) errors.lastName = "Please enter your last name";
+									if (!values.dob) errors.dob = "Please enter your DoB";
+									if (!values.streetAddress) errors.streetAddress = "Please enter your address";
+									if (!values.city) errors.city = "City is required";
+									if (!values.state) errors.state = "State is required";
+									if (!values.zip) errors.zip = "Zip is required";
+									if (!values.phone) errors.phone = "Phone is required";
+									if (!values.email) errors.email = "Email is required";
+									if (!values.race) errors.race = "Race is required";
+									if (!values.ethnicity) errors.ethnicity = "Ethnicity is required";
+									return errors;
+								}}
+								onSubmit={(values, { setSubmitting }) => {
+									setTimeout(() => {
+										console.log("here you go:", values);
+										setSubmitting(false);
+									}, 400);
+								}}
+							>
+								{({
+									values,
+									errors,
+									touched,
+									handleChange,
+									handleBlur,
+									handleSubmit,
+									isSubmitting,
+								}) => (
+									<form className='grid grid-cols-6 gap-3 gap-y-8' onSubmit={handleSubmit}>
+										{/* First Name */}
+										<div style={{ backgroundColor: touched.firstName && errors.firstName ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-3'>
+											{((touched.firstName && errors.firstName) || (touched.lastName && errors.lastName)) && <figure className='error-arrow-hldr absolute left-[-140px] top-0 translate-y-[-25%]'>
+												<img src="/error-arrow.png" className='rotate-[-90deg]' alt="" />
+											</figure>}
+											{errors.firstName && touched.firstName && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.firstName}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.firstName} name='firstName' placeholder='First Name' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* Last Name */}
+										<div style={{ backgroundColor: touched.lastName && errors.lastName ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-3'>
+											{errors.lastName && touched.lastName && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.lastName}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.lastName} name='lastName' placeholder='Last Name' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* Addr */}
+										<div style={{ backgroundColor: touched.streetAddress && errors.streetAddress ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-6'>
+											{((errors.streetAddress && touched.streetAddress)) && <figure className='error-arrow-hldr absolute left-[-140px] top-0 translate-y-[-25%]'>
+												<img src="/error-arrow.png" className='rotate-[-90deg]' alt="" />
+											</figure>}
+											{errors.streetAddress && touched.streetAddress && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.streetAddress}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.streetAddress} name='streetAddress' placeholder='Street Address' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* City */}
+										<div style={{ backgroundColor: touched.city && errors.city ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-2'>
+											{((touched.city && errors.city) || (touched.state && errors.state) || (touched.zip && errors.zip)) && <figure className='error-arrow-hldr absolute left-[-140px] top-0 translate-y-[-25%]'>
+												<img src="/error-arrow.png" className='rotate-[-90deg]' alt="" />
+											</figure>}
+											{errors.city && touched.city && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.city}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.city} name='city' placeholder='City' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* State */}
+										<div style={{ backgroundColor: touched.state && errors.state ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-2'>
+											{errors.state && touched.state && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.state}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.state} name='state' placeholder='State' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* Zip */}
+										<div style={{ backgroundColor: touched.zip && errors.zip ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-2'>
+											{errors.zip && touched.zip && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.zip}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.zip} name='zip' placeholder='Zip' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* Phone */}
+										<div style={{ backgroundColor: touched.phone && errors.phone ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-6'>
+											{((touched.phone && errors.phone)) && <figure className='error-arrow-hldr absolute left-[-140px] top-0 translate-y-[-25%]'>
+												<img src="/error-arrow.png" className='rotate-[-90deg]' alt="" />
+											</figure>}
+											{errors.phone && touched.phone && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.phone}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.phone} name='phone' placeholder='Phone' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* Email */}
+										<div style={{ backgroundColor: touched.email && errors.email ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-6'>
+											{((touched.email && errors.email)) && <figure className='error-arrow-hldr absolute left-[-140px] top-0 translate-y-[-25%]'>
+												<img src="/error-arrow.png" className='rotate-[-90deg]' alt="" />
+											</figure>}
+											{errors.email && touched.email && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.email}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.email} name='email' placeholder='Email' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* Bday */}
+										<div style={{ backgroundColor: touched.dob && errors.dob ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-6'>
+											{((touched.dob && errors.dob)) && <figure className='error-arrow-hldr absolute left-[-140px] top-0 translate-y-[-25%]'>
+												<img src="/error-arrow.png" className='rotate-[-90deg]' alt="" />
+											</figure>}
+											{errors.dob && touched.dob && <span className='inline-block absolute top-[-26px] text-[17px] font-semibold left-[-0px] text-red-600'>{errors.dob}</span>}
+											<input type="text" onChange={handleChange} onBlur={handleBlur} value={values.dob} name='dob' placeholder='Birthday (mm/dd/yyyy)' className='inline-block outline-none w-full border-none bg-transparent' />
+										</div>
+										{/* Race */}
+										<div style={{ backgroundColor: touched.race && errors.race ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-6'>
+											<p>Race</p>
+											<div className='flex flex-wrap items-start mt-[10px] gap-[20px] gap-y-[30px]'>
+												<Radio.Group onChange={handleChange} onBlur={handleBlur} size='large' name="race" className='flex items-center justify-start gap-[20px] flex-wrap ' value={values.race}>
+													<Radio value={"American Indian"}>American Indian</Radio>
+													<Radio value={"Alaskan Native"}>Alaskan Native</Radio>
+													<Radio value={"Asian"}>Asian</Radio>
+													<Radio value={"White"}>White</Radio>
+													<Radio value={"African American"}>African American</Radio>
+													<Radio value={"Native American"}>Native American</Radio>
+													<Radio value={"Pacific Islander"}>Pacific Islander</Radio>
+													<Radio value={"Other"}>Other</Radio>
+												</Radio.Group>
+											</div>
+										</div>
+										{/* Ethnicity */}
+										<div style={{ backgroundColor: touched.ethnicity && errors.race ? "red" : "#e6e7e8" }} className='bg-[#E6E7E8] relative border-[1px] border-[#000] px-[30px] py-[15.5px] col-span-6'>
+											<p>Ethnicity</p>
+											<div className='flex flex-wrap items-start mt-[10px] gap-[20px] gap-y-[30px]'>
+												<Radio.Group value={values.ethnicity} onChange={handleChange} onBlur={handleBlur} size='large' name="ethnicity" className='flex items-center justify-start gap-[20px] flex-wrap ' defaultValue={1}>
+													<Radio value={"Hispanic/Latino"}>Hispanic / Latino</Radio>
+													<Radio value={"Not Hispanic/Latino"}>Not Hispanic / Latino</Radio>
+												</Radio.Group>
+											</div>
+										</div>
+									</form>
+								)}
+							</Formik>
+							<div className='mt-[25px] flex items-center justify-end '>
+								{formToken ? (
+
+									<>
+										<div className='flex items-center justify-start flex-col bg-[#E8E7ED]'>
+											<div className='flex items-center gap-[10px] w-full py-[25px] pb-[25px] px-[35px]'>
+												<h3>Credit Card</h3>
+												<img src="/allcard.png" width={170} alt="" />
+											</div>
+											<div className='px-[35px]'>
+											<FormContainer
+												children={<h2>I am here</h2>}
+												environment="sandbox"
+												amount={23}
+												component={FormComponent}
+												clientKey={authData.clientKey}
+												apiLoginId={authData.apiLoginID}
+											/>
+											</div>
+											<div className='border-t-[1px] border-[rgba(0,0,0,0.3)] bg-[#E8E7ED] pt-[20px] px-[35px] pb-[35px]'>
+												<p className='font-normal text-[13px] mb-[10px]'>Your presonal data will be used to process your order, support your experience throughout this website, and for other purposes described in our <a className='text-red-600' href="">privacy policy</a>.</p>
+												<Checkbox onChange={() => { }}>Please check here to confirm that you have read and agree to our <a className='text-red-600' href="">terms and conditions (link opens in a new tab)*</a></Checkbox>
+											</div>
+										</div>
+
+									</>
+
+								) : (
+									<div>
+										You must have a form token. Have you made a call to the
+										getHostedPaymentPageRequestAPI?
 									</div>
-								</div>
-								{/* Ethnicity */}
-								<div className='bg-[#E6E7E8] border-[1px] border-[#000] px-[30px] py-[14.5px] col-span-6'>
-									<p>Ethnicity</p>
-									<div className='flex flex-wrap items-start mt-[10px] gap-[20px] gap-y-[30px]'>
-										<Radio.Group size='large' name="radiogroup" className='flex items-center justify-start gap-[20px] flex-wrap ' defaultValue={1}>
-											<Radio value={"American Indian"}>Hispanic / Latino</Radio>
-											<Radio value={"Alaskan Native"}>Not Hispanic / Latino</Radio>
-										</Radio.Group>
-									</div>
-								</div>
-							</form>
+								)}
+							</div>
 						</div>
 					</div>
 				</div>
